9064b71d7f698a59d43fd4b8ab7f36c1ae8431df
diegogalvalisi <117120149+diegogalvalisi@users.noreply.github.com>
2026-01-08 21:25:33 +0100

    asd

--- website/mailer/forms_settings.php
+++
@@ -1,8 +0,0 @@
-<?php
-	/* Forms settings */
-	$subj = "New message from the site 'Onovo - Portfolio Agency Template'"; //letter subject
-	$to = 'yourname@domain.com'; // Enter Your E-mail
-	$from = 'admin@domain.com'; // Admin e-mail
-	$fromName = 'Onovo'; // Your company name
-	$charset = 'UTF-8';
-?>
